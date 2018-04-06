bf2dfe52d3dbef20f5bb6b27889ad857da225e5f
Mariana Casella dos Santos <mariana.casella@88solutions.be>
2018-04-06 17:35:29 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,3 +1,26 @@
 // Your code here
 
+
+function solution(str)  {
+  if (str.length % 2 == 0) {
+    let length = str.length;
+    for (let i = 0; i <= length/2; i++ ) {
+      let holder = str[i];
+      reversedStr[i] = str[(length - 1) - i];
+      reversedStr[(length -1) - i] = holder;
+    }
+  } 
+  else {
+    let length = str.length - 1;
+    for (let i = 0; i <= length/2; i++ ) {
+      let holder = str[i];
+      reversedStr[i] = str[(length - 1) - i];
+      reversedStr[(length - 1) - i] = holder;
+    }
+  }
+  return reversedStr;
+}
+
+
+
 // Be sure it's the cleanest code you can write!
