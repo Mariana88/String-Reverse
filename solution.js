05806dda8ee45b1a092100ba8f40dcae91d18516
Mariana Casella dos Santos <mariana.casella@88solutions.be>
2018-04-08 15:51:29 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,28 +1,20 @@
 // Your code here
 
-
-function solution(str)  {
-  if (str.length % 2 == 0) {
-    let length = str.length;
-    let reversedStr ="";
-    for (let i = 0; i <= length/2; i++ ) {
-      let holder = str[i];
-      let replacer = str[(length - 1) - i];
-      // work on this - how to subtract the first character, add the last, the middle, subtract last character and add first
-      reversedStr = ;
-      console.log (reversedStr);
-      reversedStr[(length -1) - i] = holder;
-    }
-  } 
-  else {
-    let length = str.length - 1;
-    for (let i = 0; i <= length/2; i++ ) {
-      let holder = str[i];
-      reversedStr[i] = str[(length - 1) - i];
-      reversedStr[(length - 1) - i] = holder;
-    }
-  }
-  return reversedStr;
+function solution(string){
+  let beg = "";
+  let end = "";
+  let reversedStr= "";
+  let i;
+  let j;
+  for (i=0; i<= (string.length-1) / 2; i++){
+  beg = string[i] + beg
+    j = string.length - 1 - i;
+	    if (j>string.length /2){
+	    end += string[j];
+	    }
+	    reversedStr = end + beg;
+	    }
+	  return reversedStr;
 }
 
 solution("blabla");
