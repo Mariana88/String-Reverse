97d92bf668723b1dc3ee3274776c8624e25d9e4e
Mariana Casella dos Santos <mariana.casella@88solutions.be>
2018-04-08 16:34:08 +0200

    Update test-cases.js

--- tests/test-cases.js
+++ tests/test-cases.js
@@ -2,9 +2,15 @@ tests({
 
   // Fill this in with test cases for your solution
 
-  '': function() {
-    eq("", stringReverse (""));
+  'oddString': function() {
+    eq("seven", stringReverse ("neves"));
   },
-  '': function() {
-    eq("", stringReverse (""));
+  'evenString': function() {
+    eq("vivida", stringReverse ("adiviv"));
+  },
+    'mirrorString': function() {
+    eq("racecar", stringReverse ("racecar"));
+  },
+    'reoeatedString': function() {
+    eq("blabla", stringReverse ("albalb"));
   }
